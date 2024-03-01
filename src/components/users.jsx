53ae4b41fa16a05a42f964587ffa32630d22e9ef
mikhail-bn <mihail.news@gmail.com>
2024-03-02 01:20:40 +0200

    02-03-24-01

--- src/components/users.jsx
+++ src/components/users.jsx
@@ -5,15 +5,27 @@ import 'bootstrap/dist/css/bootstrap.css'
 
 
 export default function Users() {
+    // useState для массива users
     const [users, setUsers] = useState(api.users.fetchAll())
-           
+    
+    // Функция изменения массива при нажатии на кнопку
+    function handleDelete(userId) {
+        setUsers(users.filter((user) => user._id !== userId))
+    }
+
     return (       
         <>
-            <span className={"badge bg-" + (users.length > 0 ? "primary" : "dangers") }>Are you ready for the party? The {users.length} of us are waiting for you </span>
+            <h2>
+                <span
+                    className={"badge bg-" + (users.length > 0 ? "primary" : "danger")}>
+                    {users.length > 0 ? `Are you ready for the party? The ${users.length} of us ${users.length > 1 ? "are" : "is"}  waiting for you` : "You've got rid all of them"}
+                </span>
+            </h2>
+
+            {users.length > 0 &&
             <table className="table">
                 <thead>
-                    <tr>
-                        <th scope="col">#</th>
+                    <tr>                        
                         <th scope="col">Name</th>
                         <th scope="col">Personal qualities</th>
                         <th scope="col">Profession</th>
@@ -24,22 +36,21 @@ export default function Users() {
                 </thead>
                 <tbody>
                     {
-                        users.map((user, index) => {
+                        users.map((user) => {
                             return (
-                                <tr key={user._id}>                   
-                                    <td>{user[index]}</td>
+                                <tr key={user._id}>                               
                                     <td>{user.name}</td>
                                     <td>{user.qualities.map(quality => <span className={"badge m-1 bg-" + quality.color} key={quality._id}>{quality.name + " "}</span>)}</td>
                                     <td>{user.profession.name}</td>
                                     <td>{user.completedMeetings}</td>
                                     <td>{user.rate}</td>                              
-                                    <td><button className="btn btn-danger" >Get rid of</button></td>                              
+                                    <td><button className="btn btn-danger" onClick={() => handleDelete(user._id)}>Get rid of</button></td>                              
                                 </tr>  
                             )
                         })
                     }                            
                 </tbody>
-            </table>
+            </table>}
         </>
     )
 }
